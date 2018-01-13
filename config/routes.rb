1190a76bde1aa0477b7194628529737845e2ac10
Andrzej Brandt <andrzej.brandt@gmail.com>
2018-01-13 15:03:48 +0100

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  root to: 'index#welcome'
+  
+  get 'about', to: 'index#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end

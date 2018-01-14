1df79aeb9061c1a5d65da3d82b86b77c518abc27
Andrzej Brandt <andrzej.brandt@gmail.com>
2018-01-14 16:28:36 +0100

    validation of the articles implemented in the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :articles
+
   root to: 'index#welcome'
-  
   get 'about', to: 'index#about'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
